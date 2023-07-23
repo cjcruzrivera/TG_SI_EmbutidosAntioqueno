1635272cb1f562e67524f5afe28e30fdf8f5b188
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2023-07-23 14:18:39 -0500

    fix admin register

--- back/sieman/api/admin.py
+++ back/sieman/api/admin.py
@@ -1,38 +1,11 @@
 from django.contrib import admin
-from .models import (
-    Usuario,
-    MateriaPrima,
-    Producto,
-    Bodega,
-    InventarioMP,
-    InventarioPR,
-    ComposicionPR,
-    OrdenCompra,
-    MpOrdenCompra,
-    Compra,
-    Recepcion,
-    OrdenTrabajo,
-    Produccion,
-    Remision,
-    ProdsRemision,
-    Venta,
-)
+from django.apps import apps
 
-# Register your models here.
-admin.site.register(Usuario)
-admin.site.register(MateriaPrima)
-admin.site.register(Producto)
-admin.site.register(Bodega)
-admin.site.register(InventarioMP)
-admin.site.register(InventarioPR)
-admin.site.register(ComposicionPR)
-admin.site.register(OrdenCompra)
-admin.site.register(MpOrdenCompra)
-admin.site.register(Compra)
-admin.site.register(Recepcion)
-admin.site.register(OrdenTrabajo)
-admin.site.register(Produccion)
-admin.site.register(Remision)
-admin.site.register(ProdsRemision)
-admin.site.register(Venta)
+post_models = apps.get_app_config('api').get_models()
+
+for model in post_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
 
