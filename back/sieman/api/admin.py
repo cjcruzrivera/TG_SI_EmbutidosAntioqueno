4883d62146331605a2670af2a4858d26d644b605
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2023-07-23 14:05:55 -0500

    include models to admin module

--- back/sieman/api/admin.py
+++ back/sieman/api/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import (
+    Usuario,
+    MateriaPrima,
+    Producto,
+    Bodega,
+    InventarioMP,
+    InventarioPR,
+    ComposicionPR,
+    OrdenCompra,
+    MpOrdenCompra,
+    Compra,
+    Recepcion,
+    OrdenTrabajo,
+    Produccion,
+    Remision,
+    ProdsRemision,
+    Venta,
+)
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(MateriaPrima)
+admin.site.register(Producto)
+admin.site.register(Bodega)
+admin.site.register(InventarioMP)
+admin.site.register(InventarioPR)
+admin.site.register(ComposicionPR)
+admin.site.register(OrdenCompra)
+admin.site.register(MpOrdenCompra)
+admin.site.register(Compra)
+admin.site.register(Recepcion)
+admin.site.register(OrdenTrabajo)
+admin.site.register(Produccion)
+admin.site.register(Remision)
+admin.site.register(ProdsRemision)
+admin.site.register(Venta)
+
