9c630028ed02195cb66bfa354beb8f31a769ec32
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2023-07-30 20:17:36 -0500

    add parameter to formatDate to remove hours

--- front/sieman/src/utils/formatDate.js
+++ front/sieman/src/utils/formatDate.js
@@ -1,12 +1,12 @@
-const formatDate = (dateString) => {
+const formatDate = (dateString, hours = true) => {
   const date = new Date(dateString);
 
   const options = {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
+    hour: hours ? "2-digit" : undefined,
+    minute: hours ? "2-digit" : undefined,
   };
 
   const formattedDate = new Intl.DateTimeFormat("es-ES", options).format(date);
